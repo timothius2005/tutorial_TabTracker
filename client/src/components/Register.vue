4111d779002f60e246b3303a375a486c49bb0c90
Tim Woodson <timtohy.woodosn@outlook.com>
2020-07-10 07:53:34 -0700

    working front end

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
     <h1>Register</h1>
+    <input
+      type="email"
+      name="email"
+      v-model="email"
+      placeholder="email"/>
+    <br>
+    <input
+      type="password"
+      name="password"
+      v-model="password"
+      placeholder="password"/>
+      <br>
+      <button>
+      Register
+      </button>
   </div>
 </template>
 
@@ -8,12 +23,24 @@
 export default {
   data () {
     return {
+      email: '123',
+      password: '***'
 
     }
+  },
+  watch: {
+    email (value) {
+      console.log('email has changed', value)
+    }
+  },
+  mounted () {
+    setTimeout(() => {
+
+    }, 1000)
   }
 }
 </script>
 
 <style scoped>
- 
+
 </style>
